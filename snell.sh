e6ba7facc76545211ee9248185ed4f4941ba71b7
flysn0w <31747943+flysn0w@users.noreply.github.com>
2022-02-12 14:17:36 +0800

    Update snell.sh

--- snell.sh
+++ snell.sh
@@ -22,7 +22,7 @@ unzip -o snell.zip
    mkdir /etc/snell/
    echo "Generating new config..."
    echo "[snell-server]" >>${CONF}
-   echo "listen = 0.0.0.0:13254" >>${CONF}
+   echo "listen = 0.0.0.0:443" >>${CONF}
    echo "psk = ${PSK}" >>${CONF}
    echo "obfs = tls" >>${CONF}
  fi
